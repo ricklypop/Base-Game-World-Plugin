43a29f00d907ababbac44bd72933fadbbfa26486
Nick Tarsillo <nicktarsillo@Nicks-MacBook-Pro.local>
2016-07-14 11:27:05 -0400

    First commit

---
+++ Assets/Base-Game-World-Plugin/constants/BaseGameConstants.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class BaseGameConstants {
+	public const float POINTRELATIVEDISTANCE = 0.02f;
+	public const float POINTUPDATETIME = 0.25f;
+	public const float DEFAULTMOVEMENTVELOCITY = 2f;
+}
